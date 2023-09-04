00c995537d34cc3c762819fba0ebbfc2e05333a7
최재웅 <jason967@naver.com>
2023-09-05 08:55:35 +0900

    mason test ver2

--- bricks/widget/__brick__/{{name.snakeCase()}}.widget.dart
+++ bricks/widget/__brick__/{{name.snakeCase()}}.widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class {{name.pascalCase()}} extends StatelessWidget {
-const {{name.pascalCase()}}({super.key});
+class {{name.pascalCase()}}Widget extends StatelessWidget {
+const {{name.pascalCase()}}Widget({super.key});
 
 @override
 Widget build(BuildContext context) {
