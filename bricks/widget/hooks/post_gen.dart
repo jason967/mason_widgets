32b76f3c777775a2f890fa0cfa876d540736d80e
최재웅 <jason967@naver.com>
2023-09-06 15:47:51 +0900

    remove file test

--- bricks/widget/hooks/post_gen.dart
+++ bricks/widget/hooks/post_gen.dart
@@ -2,8 +2,23 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
+ _removeFiles(context, '.gitkeep');
+ _AddPackages(context, 'flutter_bloc');
+}
+
+Future<void> _removeFiles(HookContext context, String name) async {
+  final message = context.logger.progress('removing $name files');
+  final dir = Directory('.');
+  await dir.list(recursive: true).where((element) =>
+      element.toString().contains(name)).listen((element) {
+    element.delete()
+  }, onDone: () => removingFilesDone('$name files removed'));
+  progress.complete();
+}
+
+Future<void> _AddPackages(HookContext context, String name) async {
   final progress = context.logger.progress('Installing packages');
-  await Process.run('flutter', ['pub', 'add', 'flutter_bloc']);
+  await Process.run('flutter', ['pub', 'add', name]);
   await Process.run('flutter', ['pub', 'get']);
 
   progress.complete();
